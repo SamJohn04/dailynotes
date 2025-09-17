7c9958017f75643184441ce90cf7f4c7465e1dc8
SamJohn04 <samuel.john.codes@gmail.com>
2025-09-17 15:44:22 +0530

    fix: add internal/daily by removing from .gitignore

---
+++ internal/daily/new.go
@@ -0,0 +1,44 @@
+package daily
+
+func Boilerplate() string {
+	s := "DAY\n"
+	s += "\n"
+
+	s += "Gate Preparations\n"
+	s += "-----------------\n"
+	s += "What I learned today: \n"
+	s += "Explain?\n"
+	s += "\n"
+	s += "What I revised today (preferably from last week): \n"
+	s += "How well I remember it: \n"
+	s += "\n"
+
+	s += "Exercise\n"
+	s += "--------\n"
+	s += ": \n"
+	s += "\n"
+
+	s += "Chess Puzzles\n"
+	s += "-------------\n"
+	s += "Win: \n"
+	s += "Loss: \n"
+	s += "Total Puzzles: \n"
+	s += "\n"
+
+	s += "Chess Endgames\n"
+	s += "-------------\n"
+	s += "What I learned today: \n"
+	s += "Explain?\n"
+	s += "\n"
+	s += "\n"
+
+	s += "Chess Games\n"
+	s += "-----------\n"
+	s += "Win: /3\n"
+	s += "Draw: /3\n"
+	s += "Loss: /3\n"
+	s += "Total Games: /3\n"
+	s += "Rating Change: -> \n"
+
+	return s
+}
